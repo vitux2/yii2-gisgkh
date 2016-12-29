213f89d8a60fb3b6e2854a50555b47b94e4e7eb0
korshunovmaxim <korshunov.m.e@gmail.com>
2016-12-29 05:11:24 +0500

    выгрузка файлов из ГИС ЖКХ

--- models/common/GisAttachment.php
+++ models/common/GisAttachment.php
@@ -4,6 +4,7 @@ namespace opengkh\gis\models\common;
 use gisgkh\FileService;
 use gisgkh\types\lib\AttachmentType;
 use opengkh\gis\exceptions\GisgkhException;
+use opengkh\gis\exceptions\GisgkhFileDownloadException;
 use opengkh\gis\exceptions\GisgkhFileUploadException;
 
 /**
@@ -13,6 +14,19 @@ use opengkh\gis\exceptions\GisgkhFileUploadException;
  */
 class GisAttachment extends CompatibleWithGisgkh
 {
+    /* контексты хранилищ функциональных подсистем */
+    const CONTEXT_HOMEMANAGEMENT            = 'homemanagement';         // Подсистема Управление домами, Лицевые счета
+    const CONTEXT_RKI                       = 'rki';                    // Подсистема Реестр коммунальной инфраструктуры
+    const CONTEXT_VOTING                    = 'voting';                 // Подсистема Голосования
+    const CONTEXT_INSPECTION                = 'inspection';             // Подсистема Инспектирование жилищного фонда
+    const CONTEXT_INFORMING                 = 'informing';              // Подсистема Оповещения
+    const CONTEXT_BILLS                     = 'bills';                  // Подсистема Электронные счета
+    const CONTEXT_LICENSES                  = 'licenses';               // Подсистема Лицензии
+    const CONTEXT_AGREEMENTS                = 'agreements';             // Подсистема Договора (ДУ, уставы, ДПОИ)
+    const CONTEXT_NSI                       = 'nsi';                    // Подсистема Нормативно-справочная информации
+    const CONTEXT_DISCLOSURE                = 'disclosure';             // Подсистема Раскрытие деятельности УО
+    const CONTEXT_CAPITAL_REPAIR_PROGRAMS   = 'capitalrepairprograms';  // Подсистема Капитальный ремонт
+
     /**
      * @var string $name Наименование вложения
      */
@@ -69,6 +83,8 @@ class GisAttachment extends CompatibleWithGisgkh
     }
 
     /**
+     * Загрузить файл в ГИС (синхронно, до 5 Мб)
+     *
      * @param string $context контекст функциональной подсистемы
      * @param string $filePath
      * @param string $fileName
@@ -85,5 +101,26 @@ class GisAttachment extends CompatibleWithGisgkh
         }
 
         return static::convertFrom($gisAttachment);
+    }
+
+    /**
+     * Получить файл из ГИС (синхронно, до 5 Мб)
+     *
+     * @param string $context
+     * @param string $filePath путь к файлу куда нужно сохранить полученый файл
+     * @throws GisgkhFileDownloadException
+     * @return boolean
+     */
+    public function download($context, $filePath)
+    {
+        $fileService = new FileService();
+        $success = $fileService->download($context, $filePath, $this->convertTo());
+
+        if (!$success) {
+            return false;
+            // throw new GisgkhFileDownloadException($fileService->lastError);
+        }
+
+        return true;
     }
 }
